d8c82d599cc8688ebe44c51042bfd594f28aa545
Ryan Allred <ryanallred91@gmail.com>
2023-02-03 09:19:35 -0600

    Server Side setup complete

---
+++ ThatsMySong/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using ThatsMySong.Models;
+
+namespace ThatsMySong.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        void Add(UserProfile userProfile);
+    }
+}
